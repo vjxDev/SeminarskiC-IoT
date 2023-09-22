bc851ac9b9eda85adbf7960097e1de7a2c6f08df
Jovica Veljković <60753340+vjxDev@users.noreply.github.com>
2023-09-22 16:57:46 +0200

    color

--- web/src/root.tsx
+++ web/src/root.tsx
@@ -19,8 +19,8 @@ export default function Root() {
 	const location = useLocation();
 	const active = (path: string) =>
 		path == location.pathname
-			? "border-sky-600"
-			: "border-transparent hover:border-sky-600";
+			? "border-white"
+			: "border-transparent hover:border-white";
 	return (
 		<Html lang="en">
 			<Head>
@@ -31,7 +31,7 @@ export default function Root() {
 			<Body>
 				<Suspense>
 					<ErrorBoundary>
-						<nav class="bg-sky-800">
+						<nav class="bg-gray-800">
 							<ul class="container flex items-center p-3 text-gray-200">
 								<li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
 									<A href="/">Hello</A>
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,7 +2,7 @@ import { Status } from "~/components/BLEStatus";
 
 export default function Home() {
 	return (
-		<main class="grid mx-auto p-8 max-w-xl border border-blue-500 rounded-lg mt-4">
+		<main class="grid mx-auto p-8 max-w-xl border  rounded-lg mt-4">
 			<h1 class="text-2xl font-bold">Seminarski rad C </h1>
 			<p class="text-gray-600"> <b>by:</b> <a href="https://jovica.me" class="underline text-blue-500">Jovica Veljkovic</a></p>
 		</main>
